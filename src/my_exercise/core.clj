fe23958d93efda93eaddb2d2888ff2d3c6497285
Anonymous Applicant <anonymous@example.com>
2018-05-07 09:51:44 -0400

    🐧 Uncommitted changes

--- src/my_exercise/core.clj
+++ src/my_exercise/core.clj
@@ -5,8 +5,29 @@
             [ring.middleware.reload :refer [wrap-reload]]
             [my-exercise.home :as home]))
 
+(defn search [x]
+  (println x)
+  "response")
+
+(defn lcp [form field]
+  (clojure.string/lower-case (clojure.string/replace (str (get form field)) " " "_"))
+)
+
+(defn searchAddress [form]
+  (str "https://api.turbovote.org/elections/upcoming?district-divisions=ocd-division/country:us/state:" (lcp form "state") ",ocd-division/country:us/state:" (lcp form "state") "/place:" (lcp form "city"))
+)
+
+; Thanks for the opportunity to work on this. I've never used clojure before, so this is as far as I could get without going way over 2 hours.
+
+; (defn searchAddress [form]
+;   (response {:result (client/get (str "https://api.turbovote.org/elections/upcoming?district-divisions=ocd-division/country:us/state:" (lcp form "state") ",ocd-division/country:us/state:" (lcp form "state") "/place:" (lcp form "city")))})
+; )
+
 (defroutes app
   (GET "/" [] home/page)
+  ; (POST "/search" [x] (search x))
+  (POST "/search" request
+    (searchAddress (:form-params request)))
   (route/resources "/")
   (route/not-found "Not found"))
 
@@ -14,3 +35,4 @@
   (-> app
       (wrap-defaults site-defaults)
       wrap-reload))
+
